92c7721b7d1f5d7a27c2126fbee6afc0b980dbb1
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2021-05-31 15:03:36 +0530

    Delete STM32F446RE Driver directory

--- STM32F446RE Driver/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- D:/Embedded\ C/MCU\ master\ 1/STM32F446RE\ Driver/drivers/inc/stm32f446xx.h
-
-D:/Embedded\ C/MCU\ master\ 1/STM32F446RE\ Driver/drivers/inc/stm32f446xx.h:
